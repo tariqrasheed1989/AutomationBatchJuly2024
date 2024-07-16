e7838c616ed4bf08f5c42c54276e8ae0f226474a
tariq <tariq@gmail.com>
2024-07-16 21:08:10 +0530

    Stack

---
+++ src/List/StackPractice.java
@@ -0,0 +1,30 @@
+package List;
+
+import java.util.Stack;
+
+public class StackPractice {
+	
+	public static void main(String[] args) {
+		//plates system
+		
+		Stack<String> plates = new Stack<String>();
+		plates.push("plate 1");
+		plates.push("plate 2");
+		plates.push("plate 3");
+		plates.push("plate 4");
+		
+		
+//		System.out.println(plates);
+		
+//		System.out.println(plates.search("plate 3"));
+//		System.out.println(plates.peek());
+//		plates.pop();//pick or remove
+//		System.out.println(plates.peek());
+//		plates.pop();//pick or remove
+//		System.out.println(plates.peek());
+//		plates.pop();
+//		System.out.println(plates.empty());
+		
+	}
+
+}
